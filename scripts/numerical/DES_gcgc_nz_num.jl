abd04b86ba7e2c7b8a0ad6c43d8960f9d3383824
jaimerzp <jaimerz011235813@gmal.com>
2024-06-05 14:50:14 +0100

    numerical nzs

--- scripts/numerical/DES_gcgc_nz_num.jl
+++ scripts/numerical/DES_gcgc_nz_num.jl
@@ -59,7 +59,7 @@ init_params = [init_params;
     #KiDS priors
     Ωm ~ Uniform(0.2, 0.6)
     Ωbb ~ Uniform(0.28, 0.65) # 10*Ωb 
-    Ωb = 0.1*Ωbb 
+    Ωb := 0.1*Ωbb 
     h ~ Truncated(Normal(0.72, 0.05), 0.64, 0.82)
     σ8 ~ Uniform(0.4, 1.2)
     ns ~ Uniform(0.84, 1.1)
