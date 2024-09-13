eaeacaf2c6ce86b0a895f953caaf83554cf3fc0a
jaimerzp <jaimerz011235813@gmal.com>
2024-09-13 14:03:36 +0200

    scripts for numerical

--- scripts/numerical/COSMODC2_dz_3x2_num.jl
+++ scripts/numerical/COSMODC2_dz_3x2_num.jl
@@ -7,6 +7,7 @@ using YAML
 using NPZ
 using JLD2
 using PythonCall
+using Statistics
 sacc = pyimport("sacc");
 
 
@@ -14,6 +15,7 @@ method = "bpz"
 sacc_path = "../../data/CosmoDC2/summary_statistics_fourier_tjpcov.sacc"
 yaml_path = "../../data/CosmoDC2/gcgc_gcwl_wlwl.yml"
 nz_path = string("../../data/CosmoDC2/image_nzs_", method, "_priors/")
+dz_path = string("../../data/CosmoDC2/image_dz_", method, "_priors/dz_prior.npz")
 fake_data_path = string("../../data/CosmoDC2/CosmoDC2_3x2_theory_photo_", method, "_best.csv")
 
 fake_data = CSV.read(fake_data_path, DataFrame)
@@ -32,6 +34,30 @@ nz_source_1 = npzread(string(nz_path, "nz_source_1.npz"))
 nz_source_2 = npzread(string(nz_path, "nz_source_2.npz"))
 nz_source_3 = npzread(string(nz_path, "nz_source_3.npz"))
 nz_source_4 = npzread(string(nz_path, "nz_source_4.npz"))
+zs_k0, nz_k0 = nz_lens_0["z"], nz_lens_0["dndz"]
+zs_k1, nz_k1 = nz_lens_1["z"], nz_lens_1["dndz"]
+zs_k2, nz_k2 = nz_lens_2["z"], nz_lens_2["dndz"]
+zs_k3, nz_k3 = nz_lens_3["z"], nz_lens_3["dndz"]
+zs_k4, nz_k4 = nz_lens_4["z"], nz_lens_4["dndz"]
+zs_k5, nz_k5 = nz_source_0["z"], nz_source_0["dndz"]
+zs_k6, nz_k6 = nz_source_1["z"], nz_source_1["dndz"]
+zs_k7, nz_k7 = nz_source_2["z"], nz_source_2["dndz"]
+zs_k8, nz_k8 = nz_source_3["z"], nz_source_3["dndz"]
+zs_k9, nz_k9 = nz_source_4["z"], nz_source_4["dndz"]
+mu_k0 = sum(zs_k0 .* nz_k0) / sum(nz_k0)
+mu_k1 = sum(zs_k1 .* nz_k1) / sum(nz_k1)
+mu_k2 = sum(zs_k2 .* nz_k2) / sum(nz_k2)
+mu_k3 = sum(zs_k3 .* nz_k3) / sum(nz_k3)
+mu_k4 = sum(zs_k4 .* nz_k4) / sum(nz_k4)
+mu_k5 = sum(zs_k5 .* nz_k5) / sum(nz_k5)
+mu_k6 = sum(zs_k6 .* nz_k6) / sum(nz_k6)
+mu_k7 = sum(zs_k7 .* nz_k7) / sum(nz_k7)
+mu_k8 = sum(zs_k8 .* nz_k8) / sum(nz_k8)
+mu_k9 = sum(zs_k9 .* nz_k9) / sum(nz_k9)
+
+dz_prior = npzread(dz_path)
+dz_mean, dz_cov = dz_prior["mean"], dz_prior["cov"]
+dz_chol = cholesky(dz_cov).U'
 
 meta, files = make_data(sacc_file, yaml_file;
                         nz_lens_0=nz_lens_0,
@@ -78,27 +104,42 @@ init_params=[0.30, 0.5, 0.67, 0.81, 0.95]
     σ8 ~ Uniform(0.4, 1.2)
     ns ~ Uniform(0.84, 1.1)
 
+    alphas ~ filldist(Normal(0, 1), length(zs_k0))
+    SnWs = dz_mean .+ dz_chol * alphas
+    dzs := SnWs[0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+    wzs := SnWs[1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    lens_0_zs   = ((zs_k0-mu_k0)/dzs[0] + mu_k0 + wzs[0])
+    lens_1_zs   = ((zs_k1-mu_k1)/dzs[1] + mu_k1 + wzs[1])
+    lens_2_zs   = ((zs_k2-mu_k2)/dzs[2] + mu_k2 + wzs[2])
+    lens_3_zs   = ((zs_k3-mu_k3)/dzs[3] + mu_k3 + wzs[3])
+    lens_4_zs   = ((zs_k4-mu_k4)/dzs[4] + mu_k4 + wzs[4])
+    source_0_zs = ((zs_k5-mu_k5)/dzs[5] + mu_k5 + wzs[5])
+    source_1_zs = ((zs_k6-mu_k6)/dzs[6] + mu_k6 + wzs[6])
+    source_2_zs = ((zs_k7-mu_k7)/dzs[7] + mu_k7 + wzs[7])
+    source_3_zs = ((zs_k8-mu_k8)/dzs[8] + mu_k8 + wzs[8])
+    source_4_zs = ((zs_k9-mu_k9)/dzs[9] + mu_k9 + wzs[9])
+
     nuisances = Dict{String, Float64}(
         "lens_0_b"    => 0.879118,
         "lens_1_b"    => 1.05894,
         "lens_2_b"    => 1.22145,
         "lens_3_b"    => 1.35065,
         "lens_4_b"    => 1.58909,
-        "lens_0_dz"   => -0.00430633,
-        "lens_1_dz"   => -0.00316143,
-        "lens_2_dz"   => 0.00459595,
-        "lens_3_dz"   => -0.00148823,
-        "lens_4_dz"   => -0.00150412,
+        "lens_0_zs"   => lens_0_zs,
+        "lens_1_zs"   => lens_1_zs,
+        "lens_2_zs"   => lens_2_zs,
+        "lens_3_zs"   => lens_3_zs,
+        "lens_4_zs"   => lens_4_zs,
         "source_0_m"  => -0.00733846,
         "source_1_m"  => -0.00434667,
         "source_2_m"  => 0.00434908,
         "source_3_m"  => -0.00278755,
         "source_4_m"  => 0.000101118,
-        "source_0_dz" => 0.00119753,
-        "source_1_dz" => 0.00195125,
-        "source_2_dz" => -0.000356315,
-        "source_3_dz" => 0.00175369,
-        "source_4_dz" => 0.00379481)
+        "source_0_zs" => source_0_zs,
+        "source_1_zs" => source_1_zs,
+        "source_2_zs" => source_2_zs,
+        "source_3_zs" => source_3_zs,
+        "source_4_zs" => source_4_zs)
         
     cosmology = Cosmology(Ωm=Ωm, Ωb=Ωb, h=h, ns=ns, σ8=σ8,
             tk_mode=:EisHu,
@@ -119,8 +160,8 @@ println("adaptation ", adaptation)
 #println("nchains ", nchains)
 
 # Start sampling.
-folpath = "../../chains_right_nzs/nomarg/"
-folname = string("CosmoDC2_3x2_nomarg_TAP_", TAP)
+folpath = "../../chains_right_nzs/numerical/"
+folname = string("CosmoDC2_3x2_dz_num_TAP_", TAP)
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
--- scripts/numerical/COSMODC2_dz_wlwl_num.jl
+++ scripts/numerical/COSMODC2_dz_wlwl_num.jl
@@ -7,14 +7,16 @@ using YAML
 using NPZ
 using JLD2
 using PythonCall
+using Statistics
 sacc = pyimport("sacc");
 
 
 method = "bpz"
 sacc_path = "../../data/CosmoDC2/summary_statistics_fourier_tjpcov.sacc"
-yaml_path = "../../data/CosmoDC2/gcgc_gcwl_wlwl.yml"
+yaml_path = "../../data/CosmoDC2/wlwl.yml"
 nz_path = string("../../data/CosmoDC2/image_nzs_", method, "_priors/")
-fake_data_path = string("../../data/CosmoDC2/CosmoDC2_3x2_theory_photo_", method, "_best.csv")
+dz_path = string("../../data/CosmoDC2/image_dz_", method, "_priors/dz_prior.npz")
+fake_data_path = string("../../data/CosmoDC2/CosmoDC2_wlwl_theory_photo_", method, "_best.csv")
 
 fake_data = CSV.read(fake_data_path, DataFrame)
 fake_data = fake_data.theory[1:end-1]
@@ -22,35 +24,36 @@ fake_data = fake_data.theory[1:end-1]
 sacc_file = sacc.Sacc().load_fits(sacc_path)
 yaml_file = YAML.load_file(yaml_path)
 
-nz_lens_0 = npzread(string(nz_path, "nz_lens_0.npz"))
-nz_lens_1 = npzread(string(nz_path, "nz_lens_1.npz"))
-nz_lens_2 = npzread(string(nz_path, "nz_lens_2.npz"))
-nz_lens_3 = npzread(string(nz_path, "nz_lens_3.npz"))
-nz_lens_4 = npzread(string(nz_path, "nz_lens_4.npz"))
 nz_source_0 = npzread(string(nz_path, "nz_source_0.npz"))
 nz_source_1 = npzread(string(nz_path, "nz_source_1.npz"))
 nz_source_2 = npzread(string(nz_path, "nz_source_2.npz"))
 nz_source_3 = npzread(string(nz_path, "nz_source_3.npz"))
 nz_source_4 = npzread(string(nz_path, "nz_source_4.npz"))
+zs_k0, nz_k0 = nz_source_0["z"], nz_source_0["dndz"]
+zs_k1, nz_k1 = nz_source_1["z"], nz_source_1["dndz"]
+zs_k2, nz_k2 = nz_source_2["z"], nz_source_2["dndz"]
+zs_k3, nz_k3 = nz_source_3["z"], nz_source_3["dndz"]
+zs_k4, nz_k4 = nz_source_4["z"], nz_source_4["dndz"]
+mu_k0 = sum(zs_k0 .* nz_k0) / sum(nz_k0)
+mu_k1 = sum(zs_k1 .* nz_k1) / sum(nz_k1)
+mu_k2 = sum(zs_k2 .* nz_k2) / sum(nz_k2)
+mu_k3 = sum(zs_k3 .* nz_k3) / sum(nz_k3)
+mu_k4 = sum(zs_k4 .* nz_k4) / sum(nz_k4)
+
+dz_prior = npzread(dz_path)
+dz_mean, dz_cov = dz_prior["mean"], dz_prior["cov"]
+dz_mean = dz_mean[1:5]
+dz_cov = dz_cov[1:5, 1:5]
+dz_chol = cholesky(dz_cov).U'
 
 meta, files = make_data(sacc_file, yaml_file;
-                        nz_lens_0=nz_lens_0,
-                        nz_lens_1=nz_lens_1,
-                        nz_lens_2=nz_lens_2,
-                        nz_lens_3=nz_lens_3,
-                        nz_lens_4=nz_lens_4,
                         nz_source_0=nz_source_0,
                         nz_source_1=nz_source_1,
                         nz_source_2=nz_source_2,
                         nz_source_3=nz_source_3,
                         nz_source_4=nz_source_4)
 
-meta.types = [ 
-    "galaxy_density",
-    "galaxy_density",
-    "galaxy_density",
-    "galaxy_density",
-    "galaxy_density",
+meta.types = [    
     "galaxy_shear", 
     "galaxy_shear", 
     "galaxy_shear",
@@ -78,27 +81,32 @@ init_params=[0.30, 0.5, 0.67, 0.81, 0.95]
     σ8 ~ Uniform(0.4, 1.2)
     ns ~ Uniform(0.84, 1.1)
 
+    alphas ~ filldist(Normal(0, 1), length(zs_k0))
+    SnWs = dz_mean .+ dz_chol * alphas
+    dzs := SnWs[0, 2, 4, 6, 8]
+    wzs := SnWs[1, 3, 5, 7, 9]
+    source_0_zs = ((zs_k0-mu_k0)/wzs[0] + mu_k0 + dzs[0])
+    source_1_zs = ((zs_k1-mu_k1)/wzs[1] + mu_k1 + dzs[1])
+    source_2_zs = ((zs_k2-mu_k2)/wzs[2] + mu_k2 + dzs[2])
+    source_3_zs = ((zs_k3-mu_k3)/wzs[3] + mu_k3 + dzs[3])
+    source_4_zs = ((zs_k4-mu_k4)/wzs[4] + mu_k4 + dzs[4])
+
     nuisances = Dict{String, Float64}(
-        "lens_0_b"    => 0.879118,
-        "lens_1_b"    => 1.05894,
-        "lens_2_b"    => 1.22145,
-        "lens_3_b"    => 1.35065,
-        "lens_4_b"    => 1.58909,
-        "lens_0_dz"   => -0.00430633,
-        "lens_1_dz"   => -0.00316143,
-        "lens_2_dz"   => 0.00459595,
-        "lens_3_dz"   => -0.00148823,
-        "lens_4_dz"   => -0.00150412,
+        "source_0_b"    => 0.879118,
+        "source_1_b"    => 1.05894,
+        "source_2_b"    => 1.22145,
+        "source_3_b"    => 1.35065,
+        "source_4_b"    => 1.58909,
+        "source_0_zs"   => source_0_zs,
+        "source_1_zs"   => source_1_zs,
+        "source_2_zs"   => source_2_zs,
+        "source_3_zs"   => source_3_zs,
+        "source_4_zs"   => source_4_zs,
         "source_0_m"  => -0.00733846,
         "source_1_m"  => -0.00434667,
         "source_2_m"  => 0.00434908,
         "source_3_m"  => -0.00278755,
-        "source_4_m"  => 0.000101118,
-        "source_0_dz" => 0.00119753,
-        "source_1_dz" => 0.00195125,
-        "source_2_dz" => -0.000356315,
-        "source_3_dz" => 0.00175369,
-        "source_4_dz" => 0.00379481)
+        "source_4_m"  => 0.000101118)
         
     cosmology = Cosmology(Ωm=Ωm, Ωb=Ωb, h=h, ns=ns, σ8=σ8,
             tk_mode=:EisHu,
@@ -119,8 +127,8 @@ println("adaptation ", adaptation)
 #println("nchains ", nchains)
 
 # Start sampling.
-folpath = "../../chains_right_nzs/nomarg/"
-folname = string("CosmoDC2_3x2_nomarg_TAP_", TAP)
+folpath = "../../chains_right_nzs/numerical/"
+folname = string("CosmoDC2_wlwl_dz_num_TAP_", TAP)
 folname = joinpath(folpath, folname)
 
 if isdir(folname)
