e484ec6df5617f9c65bb06a0890842ea76c1de2a
jaimerzp <jaimerz011235813@gmal.com>
2024-09-13 14:30:03 +0200

    bugs

--- scripts/numerical/COSMODC2_dz_gcgc_num.jl
+++ scripts/numerical/COSMODC2_dz_gcgc_num.jl
@@ -42,8 +42,8 @@ mu_k4 = sum(zs_k4 .* nz_k4) / sum(nz_k4)
 
 dz_prior = npzread(dz_path)
 dz_mean, dz_cov = dz_prior["mean"], dz_prior["cov"]
-dz_mean = dz_mean[5:10]
-dz_cov = dz_cov[5:10, 5:10]
+dz_mean = dz_mean[10:20]
+dz_cov = dz_cov[10:20, 10:20]
 dz_chol = cholesky(dz_cov).U'
 
 meta, files = make_data(sacc_file, yaml_file;
