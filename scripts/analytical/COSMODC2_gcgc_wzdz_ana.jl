dd6acb44a64d3e7c05cae88a23a9524799053728
jaimerzp <jaimerz011235813@gmail.com>
2024-11-07 15:13:53 +0000

    wzdz

---
+++ scripts/analytical/COSMODC2_gcgc_wzdz_ana.jl
@@ -0,0 +1,157 @@
+using LinearAlgebra
+using Turing
+using LimberJack
+using CSV
+using DataFrames
+using YAML
+using NPZ
+using JLD2
+using PythonCall
+sacc = pyimport("sacc");
+
+
+method = "bpz"
+sacc_path = "../../data/CosmoDC2/summary_statistics_fourier_tjpcov.sacc"
+yaml_path = "../../data/CosmoDC2/gcgc.yml"
+nz_path = string("../../data/CosmoDC2/image_nzs_", method, "_priors/")
+cov_path = "../../covs/COSMODC2/wzdz_covs.npz"
+
+sacc_file = sacc.Sacc().load_fits(sacc_path)
+yaml_file = YAML.load_file(yaml_path)
+
+nz_lens_0 = npzread(string(nz_path, "nz_lens_0.npz"))
+nz_lens_1 = npzread(string(nz_path, "nz_lens_1.npz"))
+nz_lens_2 = npzread(string(nz_path, "nz_lens_2.npz"))
+nz_lens_3 = npzread(string(nz_path, "nz_lens_3.npz"))
+nz_lens_4 = npzread(string(nz_path, "nz_lens_4.npz"))
+
+zs_k0, nz_k0 = nz_lens_0["z"], nz_lens_0["dndz"]
+zs_k1, nz_k1 = nz_lens_1["z"], nz_lens_1["dndz"]
+zs_k2, nz_k2 = nz_lens_2["z"], nz_lens_2["dndz"]
+zs_k3, nz_k3 = nz_lens_3["z"], nz_lens_3["dndz"]
+zs_k4, nz_k4 = nz_lens_4["z"], nz_lens_4["dndz"]
+
+zs_k0, nz_k0 = LimberJack.nz_interpolate(zs_k0, nz_k0, 1000; mode="cubic")
+zs_k1, nz_k1 = LimberJack.nz_interpolate(zs_k1, nz_k1, 1000; mode="cubic")
+zs_k2, nz_k2 = LimberJack.nz_interpolate(zs_k2, nz_k2, 1000; mode="cubic")
+zs_k3, nz_k3 = LimberJack.nz_interpolate(zs_k3, nz_k3, 1000; mode="cubic")
+zs_k4, nz_k4 = LimberJack.nz_interpolate(zs_k4, nz_k4, 1000; mode="cubic")
+
+nz_lens_0 = Dict("z"=>zs_k0, "dndz"=>nz_k0)
+nz_lens_1 = Dict("z"=>zs_k1, "dndz"=>nz_k1)
+nz_lens_2 = Dict("z"=>zs_k2, "dndz"=>nz_k2)
+nz_lens_3 = Dict("z"=>zs_k3, "dndz"=>nz_k3)
+nz_lens_4 = Dict("z"=>zs_k4, "dndz"=>nz_k4)
+
+meta, files = make_data(sacc_file, yaml_file;
+                        nz_lens_0=nz_lens_0,
+                        nz_lens_1=nz_lens_1,
+                        nz_lens_2=nz_lens_2,
+                        nz_lens_3=nz_lens_3,
+                        nz_lens_4=nz_lens_4)
+
+meta.types = [ 
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density"]
+
+cov = npzread(cov_path)["gcgc_AD"]
+Γ = sqrt(cov)
+iΓ = inv(Γ)
+
+init_params=[0.30, 0.5, 0.67, 0.81, 0.95]
+
+function make_theory(;Ωm=0.27347, σ8=0.779007, Ωb=0.04217, h=0.71899, ns=0.99651,
+    meta=meta, files=files)
+    nuisances = Dict(
+        "lens_0_b"    => 0.879118,
+        "lens_1_b"    => 1.05894,
+        "lens_2_b"    => 1.22145,
+        "lens_3_b"    => 1.35065,
+        "lens_4_b"    => 1.58909,
+        "source_0_m"  => -0.00733846,
+        "source_1_m"  => -0.00434667,
+        "source_2_m"  => 0.00434908,
+        "source_3_m"  => -0.00278755,
+        "source_4_m"  => 0.000101118)
+       
+       cosmology = Cosmology(Ωm=Ωm, Ωb=Ωb, h=h, ns=ns, σ8=σ8,
+       tk_mode=:EisHu,
+       pk_mode=:Halofit,
+       nk=3000)
+
+    return Theory(cosmology, meta, files; 
+            Nuisances=nuisances,
+            int_gc="none", 
+            smooth_gc=3)
+end
+
+fake_data = make_theory();
+fake_data = iΓ * fake_data
+data = fake_data
+
+@model function model(data)
+    Ωm ~ Uniform(0.2, 0.6)
+    Ωbb ~ Uniform(0.28, 0.65) # 10*Ωb 
+    Ωb := 0.1*Ωbb 
+    h ~ Truncated(Normal(0.72, 0.05), 0.64, 0.82)
+    σ8 ~ Uniform(0.4, 1.2)
+    ns ~ Uniform(0.84, 1.1)
+
+    theory := make_theory(Ωm=Ωm, Ωb=Ωb, h=h, σ8=σ8, ns=ns)
+    ttheory = iΓ * theory
+    d = fake_data - ttheory
+    Xi2 := dot(d, d)
+    data ~ MvNormal(ttheory, I)
+end
+
+iterations = 100
+adaptation = 500
+TAP = 0.65
+init_ϵ = 0.01
+max_depth = 8
+
+println("sampling settings: ")
+println("iterations ", iterations)
+println("TAP ", TAP)
+println("adaptation ", adaptation)
+#println("nchains ", nchains)
+
+# Start sampling.
+folpath = "../../fake_chains/analytical/"
+folname = string("CosmoDC2_gcgc_bp_wzdz_ana_TAP_", TAP, "_init_ϵ_", init_ϵ) 
+folname = joinpath(folpath, folname)
+
+if isdir(folname)
+    fol_files = readdir(folname)
+    println("Found existing file ", folname)
+    if length(fol_files) != 0
+        last_chain = last([file for file in fol_files if occursin("chain", file)])
+        last_n = parse(Int, last_chain[7])
+        #println("Restarting chain")
+    else
+        #println("Starting new chain")
+        last_n = 0
+    end
+else
+    mkdir(folname)
+    println(string("Created new folder ", folname))
+    last_n = 0
+end
+
+# Create a placeholder chain file.
+CSV.write(joinpath(folname, string("chain_", last_n+1,".csv")), Dict("params"=>[]), append=true)
+
+# Sample
+cond_model = model(data)
+sampler = NUTS(adaptation, TAP; init_ϵ=init_ϵ, max_depth=max_depth)
+chain = sample(cond_model, sampler, iterations;
+                init_params=init_params,
+                progress=true, save_state=true)
+
+# Save the actual chain.       
+@save joinpath(folname, string("chain_", last_n+1,".jls")) chain
+CSV.write(joinpath(folname, string("chain_", last_n+1,".csv")), chain)
+CSV.write(joinpath(folname, string("summary_", last_n+1,".csv")), describe(chain)[1])
---
+++ scripts/numerical/COSMODC2_gcgc_wzdz_num.jl
@@ -0,0 +1,190 @@
+using LinearAlgebra
+using Turing
+using LimberJack
+using CSV
+using DataFrames
+using YAML
+using NPZ
+using JLD2
+using PythonCall
+using Statistics
+sacc = pyimport("sacc");
+
+
+method = "bpz"
+sacc_path = "../../data/CosmoDC2/summary_statistics_fourier_tjpcov.sacc"
+yaml_path = "../../data/CosmoDC2/gcgc.yml"
+nz_path = string("../../data/CosmoDC2/image_nzs_", method, "_priors/")
+dz_path = string("../../data/CosmoDC2/image_wzdz_", method, "_priors/wzdz_prior.npz")
+
+sacc_file = sacc.Sacc().load_fits(sacc_path)
+yaml_file = YAML.load_file(yaml_path)
+
+nz_lens_0 = npzread(string(nz_path, "nz_lens_0.npz"))
+nz_lens_1 = npzread(string(nz_path, "nz_lens_1.npz"))
+nz_lens_2 = npzread(string(nz_path, "nz_lens_2.npz"))
+nz_lens_3 = npzread(string(nz_path, "nz_lens_3.npz"))
+nz_lens_4 = npzread(string(nz_path, "nz_lens_4.npz"))
+zs_k0, nz_k0 = nz_lens_0["z"], nz_lens_0["dndz"]
+zs_k1, nz_k1 = nz_lens_1["z"], nz_lens_1["dndz"]
+zs_k2, nz_k2 = nz_lens_2["z"], nz_lens_2["dndz"]
+zs_k3, nz_k3 = nz_lens_3["z"], nz_lens_3["dndz"]
+zs_k4, nz_k4 = nz_lens_4["z"], nz_lens_4["dndz"]
+
+zs_k0, nz_k0 = LimberJack.nz_interpolate(zs_k0, nz_k0, 1000; mode="cubic")
+zs_k1, nz_k1 = LimberJack.nz_interpolate(zs_k1, nz_k1, 1000; mode="cubic")
+zs_k2, nz_k2 = LimberJack.nz_interpolate(zs_k2, nz_k2, 1000; mode="cubic")
+zs_k3, nz_k3 = LimberJack.nz_interpolate(zs_k3, nz_k3, 1000; mode="cubic")
+zs_k4, nz_k4 = LimberJack.nz_interpolate(zs_k4, nz_k4, 1000; mode="cubic")
+
+nz_lens_0 = Dict("z"=>zs_k0, "dndz"=>nz_k0)
+nz_lens_1 = Dict("z"=>zs_k1, "dndz"=>nz_k1)
+nz_lens_2 = Dict("z"=>zs_k2, "dndz"=>nz_k2)
+nz_lens_3 = Dict("z"=>zs_k3, "dndz"=>nz_k3)
+nz_lens_4 = Dict("z"=>zs_k4, "dndz"=>nz_k4)
+
+mu_k0 = sum(zs_k0 .* nz_k0) / sum(nz_k0)
+mu_k1 = sum(zs_k1 .* nz_k1) / sum(nz_k1)
+mu_k2 = sum(zs_k2 .* nz_k2) / sum(nz_k2)
+mu_k3 = sum(zs_k3 .* nz_k3) / sum(nz_k3)
+mu_k4 = sum(zs_k4 .* nz_k4) / sum(nz_k4)
+
+dz_prior = npzread(dz_path)
+dz_mean, dz_cov = dz_prior["mean"], dz_prior["cov"]
+dz_mean = dz_mean[11:end]
+dz_cov = dz_cov[11:end, 11:end]
+dz_chol = cholesky(dz_cov).U'
+
+meta, files = make_data(sacc_file, yaml_file;
+                        nz_lens_0=nz_lens_0,
+                        nz_lens_1=nz_lens_1,
+                        nz_lens_2=nz_lens_2,
+                        nz_lens_3=nz_lens_3,
+                        nz_lens_4=nz_lens_4)
+
+meta.types = [ 
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density",
+    "galaxy_density"]
+
+cov = meta.cov
+Γ = sqrt(cov)
+iΓ = inv(Γ)
+
+init_alphas = zeros(10)
+init_params=[0.30, 0.5, 0.67, 0.81, 0.95]
+init_params = [init_params; init_alphas]
+
+function make_theory(dzs, wzs; 
+    Ωm=0.27347, σ8=0.779007, Ωb=0.04217, h=0.71899, ns=0.99651,
+    meta=meta, files=files)
+
+    lens_0_zs = @.((zs_k0-mu_k0)/wzs[1] + mu_k0 + dzs[1])
+    lens_1_zs = @.((zs_k1-mu_k1)/wzs[2] + mu_k1 + dzs[2])
+    lens_2_zs = @.((zs_k2-mu_k2)/wzs[3] + mu_k2 + dzs[3])
+    lens_3_zs = @.((zs_k3-mu_k3)/wzs[4] + mu_k3 + dzs[4])
+    lens_4_zs = @.((zs_k4-mu_k4)/wzs[5] + mu_k4 + dzs[5])
+
+    nuisances = Dict(
+        "lens_0_b"    => 0.879118,
+        "lens_1_b"    => 1.05894,
+        "lens_2_b"    => 1.22145,
+        "lens_3_b"    => 1.35065,
+        "lens_4_b"    => 1.58909,
+        "lens_0_zs"   => lens_0_zs,
+        "lens_1_zs"   => lens_1_zs,
+        "lens_2_zs"   => lens_2_zs,
+        "lens_3_zs"   => lens_3_zs,
+        "lens_4_zs"   => lens_4_zs)
+       
+    cosmology = Cosmology(Ωm=Ωm, Ωb=Ωb, h=h, ns=ns, σ8=σ8,
+        tk_mode=:EisHu,
+        pk_mode=:Halofit,
+        nk=3000)
+
+    return Theory(cosmology, meta, files; 
+             Nuisances=nuisances,
+             int_gc="none",
+             smooth_gc=3)
+end
+
+init_dzs = zeros(5)
+init_wzs = ones(5)
+fake_data = make_theory(init_dzs, init_wzs);
+fake_data = iΓ * fake_data
+data = fake_data
+
+@model function model(data)
+    Ωm ~ Uniform(0.2, 0.6)
+    Ωbb ~ Uniform(0.28, 0.65) # 10*Ωb 
+    Ωb := 0.1*Ωbb 
+    h ~ Truncated(Normal(0.72, 0.05), 0.64, 0.82)
+    σ8 ~ Uniform(0.4, 1.2)
+    ns ~ Uniform(0.84, 1.1)
+
+    alphas ~ filldist(truncated(Normal(0, 1), -3, 3), 10)
+    SnWs = dz_mean .+ dz_chol * alphas
+    dzs := [SnWs[1], SnWs[3], SnWs[5], SnWs[7], SnWs[9]]
+    wzs := [SnWs[2], SnWs[4], SnWs[6], SnWs[8], SnWs[10]]
+
+    theory := make_theory(dzs, wzs;
+        Ωm=Ωm, Ωb=Ωb, h=h, σ8=σ8, ns=ns)
+    ttheory = iΓ * theory
+    d = fake_data - ttheory
+    Xi2 := dot(d, d)
+    data ~ MvNormal(ttheory, I)
+end
+
+iterations = 500
+adaptation = 100
+TAP = 0.65
+init_ϵ = 0.01
+max_depth = 8
+
+println("sampling settings: ")
+println("iterations ", iterations)
+println("TAP ", TAP)
+println("init_ϵ ", init_ϵ)
+println("max_depth ", max_depth)
+println("adaptation ", adaptation)
+#println("nchains ", nchains)
+
+# Start sampling.
+folpath = "../../fake_chains/numerical/"
+folname = string("CosmoDC2_gcgc_bp_wzdz_num_TAP_", TAP, "_init_ϵ_", init_ϵ)
+folname = joinpath(folpath, folname)
+
+if isdir(folname)
+    fol_files = readdir(folname)
+    println("Found existing file ", folname)
+    if length(fol_files) != 0
+        last_chain = last([file for file in fol_files if occursin("chain", file)])
+        last_n = parse(Int, last_chain[7])
+        #println("Restarting chain")
+    else
+        #println("Starting new chain")
+        last_n = 0
+    end
+else
+    mkdir(folname)
+    println(string("Created new folder ", folname))
+    last_n = 0
+end
+
+# Create a placeholder chain file.
+CSV.write(joinpath(folname, string("chain_", last_n+1,".csv")), Dict("params"=>[]), append=true)
+
+# Sample
+cond_model = model(data)
+sampler = NUTS(adaptation, TAP;
+    init_ϵ=init_ϵ, max_depth=max_depth)
+chain = sample(cond_model, sampler, iterations;
+                init_params=init_params,
+                progress=true, save_state=true)
+
+# Save the actual chain.       
+@save joinpath(folname, string("chain_", last_n+1,".jls")) chain
+CSV.write(joinpath(folname, string("chain_", last_n+1,".csv")), chain)
+CSV.write(joinpath(folname, string("summary_", last_n+1,".csv")), describe(chain)[1])
